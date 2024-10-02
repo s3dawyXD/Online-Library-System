4676f4265c9d9266c979e12e7c9198e26234c730
mohamed saad <mohamed.saad@shelter-technology.com>
2024-10-02 16:28:57 +0300

    add user views

--- online_library_system/users/api/views.py
+++ online_library_system/users/api/views.py
@@ -1,4 +1,27 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+
+
+from users.models import User
+from users.api.serializers import (
+    UserSerializer,
+    UserRegistrationSerializer,
+)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @action(
+        detail=False,
+        methods=["post"],
+        permission_classes=[],
+        serializer_class=UserRegistrationSerializer,
+    )
+    def registration(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        new_user = serializer.save()
+        return Response(UserSerializer(new_user).data, status=status.HTTP_201_CREATED)
