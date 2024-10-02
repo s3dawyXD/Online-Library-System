1aff92701b582d2be085341fe56ea37e2901ccaa
mohamed saad <mohamed.saad@shelter-technology.com>
2024-10-02 16:28:23 +0300

    add users url

--- online_library_system/online_library_system/urls.py
+++ online_library_system/online_library_system/urls.py
@@ -39,6 +39,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("books.api.urls")),
+    path("", include("users.api.urls")),
     path(
         "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
     ),
