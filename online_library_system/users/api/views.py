ca70c6ac9b32fcab3548fa01b2634241e1a76fe2
mohamed saad <mohamed.saad@shelter-technology.com>
2024-10-02 15:10:57 +0300

    initiat users app

---
+++ online_library_system/users/api/views.py
@@ -0,0 +1,4 @@
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
---
+++ online_library_system/users/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+
+
+# for custom user model
+class User(AbstractUser):
+    pass
