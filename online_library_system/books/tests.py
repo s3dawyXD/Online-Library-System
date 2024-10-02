4b2d3e06f4d927fb7cea64774d6a8050e30d156d
mohamed saad <mohamed.saad@shelter-technology.com>
2024-10-02 23:59:46 +0300

    add basic test cases

--- online_library_system/books/tests.py
+++ online_library_system/books/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from .models import Book, Author, Category
 
-# Create your tests here.
+
+class TestBooks(TestCase):
+    def setUp(self) -> None:
+        self.author = Author.objects.create(name="Test Author3")
+        self.category = Category.objects.create(name="Test Category3")
+        self.book = Book.objects.create(
+            title="Test Book",
+            author=Author.objects.create(name="Test Author"),
+            category=Category.objects.create(name="Test Category"),
+        )
+
+        self.book_2 = Book.objects.create(
+            title="Test Book 2",
+            author=Author.objects.create(name="Test Author 2"),
+            category=Category.objects.create(name="Test Category 2"),
+        )
+
+    def test_books_list(self):
+        book_count = Book.objects.count()
+        response = self.client.get("/books/list")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data.get("results")), book_count)
+
+    def test_book_detail(self):
+        response = self.client.get(f"/books/{self.book.id}/get")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["title"], "Test Book")
+
+    def test_book_create(self):
+        old_count = Book.objects.count()
+        response = self.client.post(
+            "/books/new/",
+            {
+                "title": "Test Book 3",
+                "author": self.author.id,
+                "category": self.category.id,
+            },
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Book.objects.count(), old_count + 1)
+
+    def test_book_update(self):
+        response = self.client.patch(
+            "/books/1/edit/", {"title": "Test Book 3"}, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Book.objects.get(id=self.book.id).title, "Test Book 3")
--- online_library_system/users/tests.py
+++ online_library_system/users/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class UserViewTestCase(TestCase):
+    def setUp(self):
+        self.client.post(
+            "/users/registration/",
+            {
+                "username": "testuser",
+                "email": "sJ4jv@example.com",
+                "password": "testpassword",
+            },
+        )
+
+    def test_user_view(self):
+        response = self.client.get("/users/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_registration(self):
+        response = self.client.post(
+            "/users/registration/",
+            {
+                "username": "testuser1",
+                "email": "sJ4jvv@example.com",
+                "password": "testpassword",
+            },
+        )
+        self.assertEqual(response.status_code, 201)
