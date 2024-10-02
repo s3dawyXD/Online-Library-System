bcbb8a326aed39eeba933488ba58a5fc51b632d9
mohamed saad <mohamed.saad@shelter-technology.com>
2024-10-02 16:28:49 +0300

    add user serilaizer

--- online_library_system/users/api/serializers.py
+++ online_library_system/users/api/serializers.py
@@ -1,1 +1,42 @@
 from rest_framework import serializers
+from users.models import User
+from django.contrib.auth.password_validation import validate_password
+
+
+class UserRegistrationSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    email = serializers.EmailField()
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.is_active = True
+        user.save()
+        return user
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("User with this username already exists")
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("User with this email already exists")
+        return value
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "is_active",
+        ]
