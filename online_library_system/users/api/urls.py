cc09a359136a24a476e626422629d5f3d8a7cdae
mohamed saad <mohamed.saad@shelter-technology.com>
2024-10-02 16:28:14 +0300

    add login route

--- online_library_system/users/api/urls.py
+++ online_library_system/users/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from users.api.views import UserViewSet
+from dj_rest_auth.views import LoginView
 
 router = DefaultRouter()
 
+router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
+    path("login/", LoginView.as_view(), name="login"),
     path("", include(router.urls)),
 ]
